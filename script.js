0c87884a527c9730bc04336cb13977959bed39af
Sam <sam@sambarker.com>
2021-03-31 14:46:07 +0100

    add correct filesgit add .

--- script.js
+++ script.js
@@ -1,55 +1,33 @@
-//Stuff we will change when we clock the myBtn
-stuffToChange = () => {
-    document.getElementById("my-h1").innerHTML = "Goodbye World";
-    document.getElementById("my-text").style.color = "blue";
+// Get arrays for buttons and images
+const buttons = document.querySelectorAll('.buttonFilter');
+const images = document.querySelectorAll('.imageFilter');
 
-    let para = document.getElementsByClassName("my-p");
-    for (let item of para) {
-        item.style.color = "green";
-    }
-
-    let newPara = document.createElement("p");
-    let node = document.createTextNode("This is a new paragraph");
-    newPara.appendChild(node);
-    let element = document.getElementById("main-content");
-    element.append(newPara);
-
-    document.getElementById("myBtn").style.display = "none";
-}
-
-// Change the stuff as specified in stuffToChange function
-document.getElementById("myBtn").addEventListener("click", stuffToChange);
-
-// When hovering over hover-p, alert hello
-document.getElementById("hover-p").addEventListener("mouseover", () => {
-    alert("Hello");
-});
-
-// When clicking on the link, do not follow link. Instead, alert link blocked
-document.getElementById("my-link").addEventListener("click", (event) => {
-    event.preventDefault();
-    alert("Link blocked")
-});
-
-// Change the myButton styling when it is clicked
-let myButton = document.getElementById("click-it").addEventListener("click", (event) => {
-    let btn = event.currentTarget;
-    btn.style.backgroundColor = "red";
-    btn.innerHTML = "I have been clicked"
+// Filter using buttons
+buttons.forEach(item => {
+    item.addEventListener("click", () => {
+        const animal = item.getAttribute('animal');
+        for (let image of images) {
+            image.setAttribute('style', 'display:inline-block');
+            if (animal != "all" && !image.getAttribute('class').includes(animal)){
+                image.setAttribute('style', 'display:none');
+            }
+        }
+    })
 })
 
-// When submitBtn is clicked, get first name and last name and alert
-let submitBtn = document.getElementById("submitBtn").addEventListener("click", (event) => {
-    let fname = document.getElementById("fname").value;
-    let lname = document.getElementById("lname").value;
-
-    let radios = document.getElementsByName("gender");
-    let radioValue;
-    for (let i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            radioValue = radios[i].value;
-            console.log(radioValue)
+// Filter using search box
+document.getElementById("filter").addEventListener("input", () => {
+    let searchTerm = document.getElementById("filter").value;
+    if (!searchTerm) {
+        for (let image of images) {
+            image.setAttribute('style', 'display:inline-block');
+        }
+    } else {
+        for (image of images) {
+            let showHide = image.getAttribute('style');
+            if (!image.getAttribute('alt').includes(searchTerm) && showHide === 'display:inline-block') {
+                image.setAttribute('style', 'display:none');
+            }
         }
     }
-    alert(`Hello ${fname} ${lname}! Your gender is ${radioValue}.`)
 })
